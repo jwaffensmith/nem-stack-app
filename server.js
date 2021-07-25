580ca302e87c46ee3d4324c3bfd25bbbbd04fb60
Username <jwaffensmith@gmail.com>
2021-07-25 15:04:18 -0700

    dependencies installedand server up and running

---
+++ server.js
@@ -0,0 +1,16 @@
+// DB Connection
+
+// external modules 
+const express = require("express");
+
+// instance express package
+const app = express();
+
+// Port connection
+const PORT = 4000;
+
+// Bind server to port
+app.listen(PORT, function () {
+    console.log(`Hey, I am alive and well at port ${PORT}!`);
+});
+
