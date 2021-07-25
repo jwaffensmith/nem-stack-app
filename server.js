1800ed65d78bea49dd831c0ce4e0f7c4bc906938
Username <jwaffensmith@gmail.com>
2021-07-25 16:50:07 -0700

    added middleware and mapped out the routes

--- server.js
+++ server.js
@@ -10,10 +10,31 @@ const app = express();
 
 // Port connection
 const PORT = 4000;
+app.set("view engine", "ejs");
 
 // internal modules
 const Hike = require("./models/Hike");
 const hikeData = require("./db/seed");
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+// Index GET /hikes
+
+// New GET /hikes/new
+
+// Create POST /hikes
+
+// Show GET /hikes/:id
+
+// Edit GET /hikes/:id/edit
+
+// Update PUT /hikes/:id
+
+// Destroy DELETE /hikes/:id
 
 
 // Bind server to port
